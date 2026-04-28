1a69df9df11098431d0f0dbbd47672aa08229480
Милена <mylkkiko16@gmail.com>
2026-04-28 10:26:40 +0300

    Iterator

--- Iterator/iterator.ts
+++ Iterator/iterator.ts
@@ -6,89 +6,19 @@ interface ITrack {
     addedAt: Date;
 }
 
-interface IteratorRes<T> {
-    value?: T;
-    done: boolean;
-}
-
 class Track implements ITrack {
-    public name: string;
-    public singer: string;
-    public duration: number;
-    public numberOfAuditions: number;
-    public addedAt: Date;
-
     constructor(
-        name: string, 
-        singer: string, 
-        duration: number,
-        numberOfAuditions: number, 
-        addedAt?: Date
-    ) {
-        this.name = name;
-        this.singer = singer;
-        this.duration = duration;
-        this.numberOfAuditions = numberOfAuditions;
-        this.addedAt = addedAt || new Date();
-    }
-}
-
-abstract class PlaylistIterator {
-    protected items: Track[];
-    protected index: number = 0;
-
-    constructor(collection: Playlist) {
-        this.items = [...collection.getTracks()];
-    }
-
-    protected hasNext(): boolean {
-        return this.index < this.items.length;
-    }
-
-    public next(): IteratorRes<Track> {
-        if(this.hasNext()) {
-            return {
-                value: this.items[this.index++],
-                done: false
-            };
-        }
-        return {done: true};
-    }
-}
-
-class DateAddedIterator extends PlaylistIterator {
-    constructor(collection: Playlist) {
-        super(collection);
-        this.sortDate();
-    }
-    private sortDate(): void {
-        this.items.sort((a, b) => a.addedAt.getTime() - b.addedAt.getTime());
-    }
-}
-
-class DurationIterator extends PlaylistIterator {
-    constructor(collection: Playlist) {
-        super(collection);
-        this.sortDuration();
-    }
-    private sortDuration(): void {
-        this.items.sort((a, b) => a.duration - b.duration);
-    }
-}
-
-class PopularityIterator extends PlaylistIterator {
-    constructor(collection: Playlist) {
-        super(collection);
-        this.sortPopularity();
-    }
-    private sortPopularity(): void {
-        this.items.sort((a, b) => a.numberOfAuditions - b.numberOfAuditions);
-    }
+        public readonly name: string,
+        public readonly singer: string,
+        public readonly duration: number,
+        public readonly numberOfAuditions: number,
+        public readonly addedAt: Date = new Date()
+    ) {}
 }
 
 class Playlist {
     private tracks: Track[] = [];
-    public name: string;
+    public name: string; 
 
     constructor(name: string) {
         this.name = name;
@@ -99,35 +29,75 @@ class Playlist {
     }
 
     public getTracks(): Track[] {
-        return this.tracks;
+        return [...this.tracks];
     }
 
-    public getIteratorByDate(): DateAddedIterator{
+    public getIteratorByDate(): DateAddedIterator {
         return new DateAddedIterator(this);
     }
 
-    public getIteratorByDuration(): DurationIterator{
+    public getIteratorByDuration(): DurationIterator {
         return new DurationIterator(this);
     }
 
-    public getIteratorByPopularity(): PopularityIterator{
+    public getIteratorByPopularity(): PopularityIterator {
         return new PopularityIterator(this);
     }
 }
 
-function displayIteratorResults<T> (
+abstract class PlaylistIterator implements Iterator<Track> {
+    protected items: Track[];
+    private index: number = 0;
+
+    constructor(collection: Playlist) {
+        this.items = [...collection.getTracks()];
+        this.sort();
+    }
+
+    protected abstract sort(): void;
+
+    private hasNext(): boolean {
+        return this.index < this.items.length;
+    }
+
+    public next(): IteratorResult<Track> {
+        if (this.hasNext()) {
+            return { value: this.items[this.index++], done: false };
+        }
+        return { value: undefined, done: true };
+    }
+
+    [Symbol.iterator](): Iterator<Track> {
+        return this;
+    }
+}
+
+class DateAddedIterator extends PlaylistIterator {
+    protected sort(): void {
+        this.items.sort((a, b) => a.addedAt.getTime() - b.addedAt.getTime());
+    }
+}
+
+class DurationIterator extends PlaylistIterator {
+    protected sort(): void {
+        this.items.sort((a, b) => a.duration - b.duration);
+    }
+}
+
+class PopularityIterator extends PlaylistIterator {
+    protected sort(): void {
+        this.items.sort((a, b) => a.numberOfAuditions - b.numberOfAuditions);
+    }
+}
+
+function displayIteratorResults<T>(
     name: string,
-    iterator: { next(): { value?: T; done: boolean } },
+    iterator: Iterable<T>,       
     formatFn: (item: T) => string
 ): void {
     console.log(`\n--- ${name} ---`);
-    let result = iterator.next();
-
-    while(!result.done) {
-        if(result.value) {
-            console.log(formatFn(result.value));
-        }
-        result = iterator.next();
+    for (const item of iterator) {
+        console.log(formatFn(item));
     }
 }
 
@@ -143,19 +113,19 @@ myPlaylist.addTrack(track3);
 myPlaylist.addTrack(track4);
 
 displayIteratorResults(
-    "Сортировка по дате (от старых к новым)", 
-    myPlaylist.getIteratorByDate(), 
+    "Сортировка по дате (от старых к новым)",
+    myPlaylist.getIteratorByDate(),
     (t: Track) => `[${t.addedAt.toLocaleDateString('ru-RU')}] ${t.singer} - ${t.name}`
 );
 
 displayIteratorResults(
-    "Сортировка по длительности (по возрастанию)", 
-    myPlaylist.getIteratorByDuration(), 
+    "Сортировка по длительности (по возрастанию)",
+    myPlaylist.getIteratorByDuration(),
     (t: Track) => `${t.duration} сек. | ${t.name} (${t.singer})`
 );
 
 displayIteratorResults(
-    "Сортировка по популярности (от менее популярных)", 
-    myPlaylist.getIteratorByPopularity(), 
+    "Сортировка по популярности (от менее популярных)",
+    myPlaylist.getIteratorByPopularity(),
     (t: Track) => `${t.numberOfAuditions} прослушиваний | ${t.name}`
 );
